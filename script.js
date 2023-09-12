ef531152e862725071d8d5141f69fdec8c568ff5
ersomenki <heewkakupuewka@gmail.com>
2023-09-12 15:07:19 +0600

    donedone

--- script.js
+++ script.js
@@ -1,69 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const previousButton = document.getElementById('previousButton');
-  const nextButton = document.getElementById('nextButton');
-  const sliderContent = document.querySelector('.slider-content');
-  const slides = sliderContent.querySelectorAll('img');
-  const sliderDots = document.querySelector('.slider-dots');
+document.addEventListener("DOMContentLoaded", () => {
+  // Configuration settings for the slider
+  const sliderConfig = {
+    slideWidthPercent: 100,
+    transitionDuration: 500,
+    cloneDuration: 550,
+    currentIndex: 0
+  };
+  // Slider body and variables
+  const slider = {
+    previousButton: document.getElementById("previousButton"),
+    nextButton: document.getElementById("nextButton"),
+    sliderContent: document.querySelector(".slider-content"),
+    slides: document.querySelectorAll(".slider-content img"),
+    sliderDots: document.querySelector(".slider-dots"),
+    dots: [],
+    activeDot: null,
+    copyOfLastSlide: null,
+    copyOfFirstSlide: null,
+    isAnimating: false
+  };
 
-  const copyOfLastSlide = slides[slides.length - 1].parentElement.cloneNode(true);
-  const copyOfFirstSlide = slides[0].parentElement.cloneNode(true);
+  
+  // Function to initialize the slider
+  function initSlider() {   
+    slider.sliderContent.style.transform = `translateX(-100%)`; // Initialize first slide without animation
+    drawDots();
+    goToSlide(sliderConfig.currentIndex);
 
-  sliderContent.prepend(copyOfLastSlide);
-  sliderContent.append(copyOfFirstSlide);
+     // Set event listeners for nav buttons
+    slider.previousButton.addEventListener("click", () =>
+      goToSlide(sliderConfig.currentIndex - 1)
+    );
+    slider.nextButton.addEventListener("click", () =>
+      goToSlide(sliderConfig.currentIndex + 1)
+    );
 
-  const slideWidthPercent = 100;
-  const animationDuration = 500;
-  const cloneDuration = 530;
-
-  let currentIndex = 0;
-  let dots = [];
-  let activeDot;
-  let isAnimating = false;
-
-  drawDots();
-  goToSlide(0);
-
-  previousButton.addEventListener('click', () => goToSlide(currentIndex - 1));
-  nextButton.addEventListener('click', () => goToSlide(currentIndex + 1));
+    // Clone the last and first slides for the illusion of infinity
+    slider.copyOfLastSlide = slider.slides[slider.slides.length - 1].parentElement.cloneNode(true);
+    slider.copyOfFirstSlide = slider.slides[0].parentElement.cloneNode(true);
+    slider.sliderContent.prepend(slider.copyOfLastSlide);
+    slider.sliderContent.append(slider.copyOfFirstSlide);
+  }
 
   function goToSlide(index) {
-    if (isAnimating) {
+    if (slider.isAnimating) {
       return;
     }
 
-    isAnimating = true;
+    slider.isAnimating = true;
 
-    updateSlider((slideWidthPercent + index * slideWidthPercent) * -1);
-
+    // Slides of value and steps
+    updateSlider(
+      (sliderConfig.slideWidthPercent + index * sliderConfig.slideWidthPercent) *
+        -1
+    );
+    // If end of slides
     if (index < 0) {
-      currentIndex = slides.length - 1;
-    } else if (index > slides.length - 1) {
-      currentIndex = 0;
+      sliderConfig.currentIndex = slider.slides.length - 1;
+    } else if (index > slider.slides.length - 1) {
+      sliderConfig.currentIndex = 0;
     } else {
-      currentIndex = index;
+      sliderConfig.currentIndex = index;
     }
-    
-    
+
+
+    // Pos for the last slides without animation
     setTimeout(() => {
-      if (index === -1) {
-        updateSliderNoInterval(slides.length * slideWidthPercent * -1);
+      if (index === -1 && slider.isAnimating === true) {
+        updateSliderNoInterval(
+          slider.slides.length * sliderConfig.slideWidthPercent * -1
+        );
       }
-      if (index === slides.length) {
-        updateSliderNoInterval(-slideWidthPercent);
+      if (index === slider.slides.length && slider.isAnimating === true) {
+        updateSliderNoInterval(-sliderConfig.slideWidthPercent);
       }
-      isAnimating = false;
-      setActiveDot(dots[currentIndex]);
-    }, cloneDuration);
-  }
-  
-  function updateSliderNoInterval(value) {
-    sliderContent.style.transform = `translateX(${value}%)`;
+      slider.isAnimating = false;
+      setActiveDot(slider.dots[sliderConfig.currentIndex]);
+    }, sliderConfig.cloneDuration);
   }
 
+  // Function to update the slider's position without interval animation
+  function updateSliderNoInterval(value) {
+    slider.sliderContent.style.transform = `translateX(${value}%)`;
+  }
+  // Update slides with setInterval
   function updateSlider(value) {   
     const initialValue = 
     parseFloat(
-      sliderContent.style.transform
+      slider.sliderContent.style.transform
       .replace('translateX(', '')
       .replace('%)', '')) || 0;
     const targetValue = value;
@@ -73,40 +97,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const interval = setInterval(() => {
       if (currentStep < steps) {
         const newValue = initialValue + stepValue * currentStep;
-        sliderContent.style.transform = `translateX(${newValue}%)`;
+        slider.sliderContent.style.transform = `translateX(${newValue}%)`;
         currentStep++;
        } else {
-        sliderContent.style.transform = `translateX(${targetValue}%)`;
+        slider.sliderContent.style.transform = `translateX(${targetValue}%)`;
         clearInterval(interval);
       }
-    },animationDuration / steps);
+    },sliderConfig.transitionDuration / steps);
 
   }
-
+  // Function to set the active dot in the slider navigation
   function setActiveDot(dot) {
-    if (activeDot) {
-      activeDot.classList.remove('active');
+    if (slider.activeDot) {
+      slider.activeDot.classList.remove("active");
     }
-
-    activeDot = dot;
-    activeDot.classList.add('active');
+    slider.activeDot = dot;
+    slider.activeDot.classList.add("active");
   }
 
+  // Function to create and display slider navigation dots
   function drawDots() {
-    slides.forEach(() => {
-      const element = Object.assign(document.createElement('div'), {        
-        className: 'slider-dots-item'
-      });
-      dots.push(element);
-      sliderDots.append(element);
+    slider.slides.forEach(() => {
+      const element = document.createElement("div");
+      element.classList.add("slider-dots-item");
+      slider.dots.push(element);
+      slider.sliderDots.append(element);
     });
   }
-  sliderDots.addEventListener('click', (event) => {
-    if (!isAnimating && event.target.classList.contains('slider-dots-item')) {
-      const index = dots.indexOf(event.target);
+  
+  // Add a click event listener to the slider navigation dots
+  slider.sliderDots.addEventListener("click", (event) => {
+    if (!slider.isAnimating && event.target.classList.contains("slider-dots-item")) {
+      const index = slider.dots.indexOf(event.target);
       if (index !== -1) {
         goToSlide(index);
       }
     }
   });
+
+  // Initialize the slider when the DOM content is loaded
+  initSlider();
 });
